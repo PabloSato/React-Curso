acb56bcd2e956cb5c24cbc6491bba005be5b9932
Pablo Fernández Sato <pablofersato@gmail.com>
2023-08-16 12:09:42 +0200

    END challenge_02

--- challenges/section_01/02_challenge/src/index.js
+++ challenges/section_01/02_challenge/src/index.js
@@ -2,6 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 
+const skills = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: '#2662EA',
+  },
+  {
+    skill: 'JavaScript',
+    level: 'advanced',
+    color: '#EFD81D',
+  },
+  {
+    skill: 'Web Design',
+    level: 'advanced',
+    color: '#C3DCAF',
+  },
+  {
+    skill: 'Git and GitHub',
+    level: 'intermediate',
+    color: '#E84F33',
+  },
+  {
+    skill: 'React',
+    level: 'advanced',
+    color: '#60DAFB',
+  },
+  {
+    skill: 'Svelte',
+    level: 'beginner',
+    color: '#FF3B00',
+  },
+];
+
 function App() {
   return (
     <div className='card'>
@@ -37,19 +70,22 @@ function Intro() {
 function SkillList() {
   return (
     <div className='skill-list'>
-      <Skill skill='React' emoji='💪' color='blue' />
-      <Skill skill='HTML+CSS' emoji='💪' color='orange' />
-      <Skill skill='JavaScript' emoji='💪' color='yellow' />
-      <Skill skill='PHP' emoji='👶' color='orangered' />
+      {skills.map((skill) => (
+        <Skill skillObj={skill} />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skillObj }) {
   return (
-    <div className='skill' style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className='skill' style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill}</span>
+      <span>
+        {skillObj.level === 'beginner' && '👶'}
+        {skillObj.level === 'intermediate' && '👍'}
+        {skillObj.level === 'advanced' && '💪'}
+      </span>
     </div>
   );
 }
