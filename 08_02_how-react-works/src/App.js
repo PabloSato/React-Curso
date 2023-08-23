ec62c18491fa9711d3ed7be117cc4720a2887006
Pablo Fernández Sato <pablofersato@gmail.com>
2023-08-23 12:25:18 +0200

    add handlefunctions on how_react_works

--- 08_02_how-react-works/src/App.js
+++ 08_02_how-react-works/src/App.js
@@ -66,7 +66,25 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    // setLikes(likes + 1); => NO WORKINGS
+    // Si lo hacemos con una funcion SI QUE TENEMOS acceso al último valor actualizado del state
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater() {
+    // Ejecuta la función 2 segundos (2000 milisecs) después de ser llamada
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -82,13 +100,13 @@ function TabContent({ item }) {
         <div className='hearts-counter'>
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className='tab-undo'>
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
