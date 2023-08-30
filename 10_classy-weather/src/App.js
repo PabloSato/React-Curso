404ea7974c19d3483d2878254697bc76ba61588f
Pablo Fernández Sato <pablofersato@gmail.com>
2023-08-30 10:08:29 +0200

    stop for now - LEGACY!

--- 10_classy-weather/src/App.js
+++ 10_classy-weather/src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div></div>;
+import React from 'react';
+
+// Así se trabaja antes con CLASES (esto está 'deprecado')
+class Counter extends React.Component {
+  // Para poder usar el State hay que usar el constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+
+  // Siempre hay que incluir el método render()
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
